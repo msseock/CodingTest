71be0f54b799ecaa1794f1335c0ad86d04373f81
msseock <msseock@gmail.com>
2023-07-03 21:45:51 +0900

    백준 1673 개선
    치킨 쿠폰 문제 - recursion을 이용한 방식에서 while을 사용한 방식으로 개선하여 코드 간략화, 시간 단축

--- Swift/1673.swift
+++ Swift/1673.swift
@@ -1,30 +1,20 @@
 import Foundation
 
 var coupon: Int
-var stamp_per_chicken: Int
+var stamp: Int
 var chicken_max: Int
 
 while let line = readLine() {
     let input = line.components(separatedBy: " ").map { Int($0)! }
     coupon = input[0]
-    stamp_per_chicken = input[1]
+    stamp = input[1]
 
     chicken_max = coupon
 
-    func chicken(stamp: Int, stamp_per_chicken: Int) -> Int {
-        
-        guard stamp >= stamp_per_chicken  else {
-            return 0
-        }
-        
-        let coupon = stamp / stamp_per_chicken
-        
-        let stamp_remaining = stamp/stamp_per_chicken + stamp % stamp_per_chicken
-        
-        return coupon + chicken(stamp: stamp_remaining, stamp_per_chicken: stamp_per_chicken)
+    while coupon >= stamp {
+        chicken_max += coupon/stamp
+        coupon = coupon / stamp + coupon % stamp
     }
-
-    chicken_max += chicken(stamp: coupon, stamp_per_chicken: stamp_per_chicken)
     
     print(chicken_max)
 
